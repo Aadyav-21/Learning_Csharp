5b32769a611ec8ec35ce29b3be8b549c9c745b9b
Aadya Vibhuti <135000328+Aadyav-21@users.noreply.github.com>
2023-10-05 08:47:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.Write("Aadya loves");
+//Console.WriteLine(" Harish");
+//typecasting
+int a = 2;
+float b = (float)a;
+Console.WriteLine("aadya" + a);
+Console.WriteLine("enter ur name");
+string name = Console.ReadLine();
+Console.WriteLine(name.Length);
+Console.WriteLine("hello" + name);
+//if-else
+Console.WriteLine("enter ur age");
+string c = Console.ReadLine();
+int age = Convert.ToInt32(c);
+if (age > 18)
+{
+    Console.WriteLine("ELIGIBLE");
+}
+Console.ReadLine();
+
